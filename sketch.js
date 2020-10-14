3415745b2f62b18cbe8ba7bf837c1447b9caa7be
Prajwolbh7 <60246105+Prajwolbh7@users.noreply.github.com>
2020-10-14 12:02:48 -0700

    added text

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ let x = 1;
 let y = 1;
 let easing = 0.05;
 
+
 let rad = 60; // Width of the shape
 let xpos, ypos; // Starting position of shape
 
@@ -14,6 +15,7 @@ let ydirection = 1; // Top to Bottom
 
 
 function setup() {
+
   createCanvas(1000, 600);
   //background(255,0,133);
   background(255);
@@ -24,63 +26,82 @@ function setup() {
   // Set the starting position of the shape
   xpos = width / 2;
   ypos = height / 2;
-
+  fill(0);
+  textSize(34);
+  text('Click to Start', 400,300);
 
 }
 
 function draw() {
 
 
-       //background(0);
-    // it draws as the mouse is moving without being pressed
-      // if (!mouseIsPressed){
-      //   ellipse(mouseX, mouseY, 20,20);
-      // }
-      if (mouseIsPressed){
-        background(0);
-        //background(0);
-   // Update the position of the shape
-  xpos = xpos + xspeed * xdirection;
-  ypos = ypos + yspeed * ydirection;
-      }
-else if(!mouseIsPressed) {
-
-  // Draw the shape
-  fill(290);
-  textSize(24);
-  text('1',600,400);
-  text('2',900,50);
-  text('1',100,100);
-//  ellipse(10, 10, 5, 5);
+  //background(0);
+  // it draws as the mouse is moving without being pressed
+  // if (!mouseIsPressed){
+  //   ellipse(mouseX, mouseY, 20,20);
+  // }
+  if (mouseIsPressed) {
+    background(0);
+    //background(0);
+    fill('#fcba03');
+    // Update the position of the shape
+    xpos = xpos + xspeed * xdirection;
+    ypos = ypos + yspeed * ydirection;
 
 
+    // Draw the shape
+    push();
+    fill(290);
+    textSize(24);
+    fill(600);
+     //ellipse(600, 400, 20, 20);
+     ellipse(random(200), random(400), 15, 15);
+     ellipse(random(300), random(500), 15, 15);
+     ellipse(random(400), random(400), 15, 15);
+     ellipse(random(500), random(500), 15, 15);
+     ellipse(random(600), random(600), 15, 15);
+     ellipse(random(700), random(600), 15, 15);
+     ellipse(random(800), random(600), 15, 15);
+     ellipse(random(900), random(600), 15, 15);
+     ellipse(random(900), random(600), 15, 15);
+     ellipse(random(900), random(600), 15, 15);
+     ellipse(random(900), random(600), 15, 15);
+     ellipse(random(900), random(600), 15, 15);
+     ellipse(random(500), random(500), 15, 15);
+     ellipse(random(600), random(600), 15, 15);
+     ellipse(random(700), random(600), 15, 15);
+     ellipse(random(800), random(600), 15, 15);
+     ellipse(random(900), random(600), 15, 15);
+     ellipse(random(900), random(600), 15, 15);
+     ellipse(random(900), random(600), 15, 15);
+     ellipse(random(900), random(600), 15, 15);
+     ellipse(random(900), random(600), 15, 15);
+     pop();
 
-
-        //stroke(map(mouseX,0,600,0,255,true));
-        //line(width-mouseX, height- mouseY, width-pmouseX, height-pmouseY); // width - makes it mirror the drawing
-          //ellipse(mouseX, mouseY, pmouseX,pmouseY); // pmouseX is previous cursor
-         // fill(0);
-        //ellipse(mouseX, mouseY, 5,5); // pmouseX is previous cursor
-
+    //stroke(map(mouseX,0,600,0,255,true));
+    //line(width-mouseX, height- mouseY, width-pmouseX, height-pmouseY); // width - makes it mirror the drawing
+    //ellipse(mouseX, mouseY, pmouseX,pmouseY); // pmouseX is previous cursor
+    // fill(0);
+    //ellipse(mouseX, mouseY, 5,5); // pmouseX is previous cursor
 }
 
- let targetX = mouseX;
+  let targetX = mouseX;
   let dx = targetX - x;
   x += dx * easing;
 
   let targetY = mouseY;
   let dy = targetY - y;
   y += dy * easing;
-
+//  fill(random(0-255));
   ellipse(x, y, 20, 20);
 }
 
-function keyTyped(){
+function keyTyped() {
 
-    if (key === 's'){
-      // save this image
-      saveCanvas('fileName', 'png');
+  if (key === 's') {
+    // save this image
+    saveCanvas('fileName', 'png');
 
-    }
-    return false;
+  }
+  return false;
 }
