51f59da3abee92ed1fd719006708ccc7fba9be7d
Prajwolbh7 <60246105+Prajwolbh7@users.noreply.github.com>
2020-10-20 17:29:41 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,23 +30,23 @@ function setup() {
   ypos = height / 2;
   fill(255);
   textSize(34);
-  text('Click to Start', 600,300);
+  text('Click to Start', 600, 300);
 
 }
 
 function draw() {
-background(0,0,0,4);
-strokeWeight(strokeWidth);
+  background(0, 0, 0, 4);
+  strokeWeight(strokeWidth);
 
 
-//background(0,0,0,20);
+  //background(0,0,0,20);
   // it draws as the mouse is moving without being pressed
   // if (!mouseIsPressed){
   //   ellipse(mouseX, mouseY, 20,20);
   // }
 
-noiseOffset+= 0.04;
-strokeWidth = noise(noiseOffset) * 5;
+  noiseOffset += 0.04;
+  strokeWidth = noise(noiseOffset) * 5;
 
 
   if (mouseIsPressed) {
@@ -62,39 +62,39 @@ strokeWidth = noise(noiseOffset) * 5;
 
     // Draw the shape
     push();
-    fill(600);
-    textSize(24);
-   fill('#8fffff');
-     //ellipse(600, 400, 20, 20);
-     ellipse(random(200), random(400), 15, 15);
-     ellipse(random(300), random(500), 15, 15);
-     ellipse(random(400), random(400), 15, 15);
-     ellipse(random(500), random(500), 15, 15);
-     ellipse(random(600), random(600), 15, 15);
-     ellipse(random(700), random(600), 15, 15);
-     ellipse(random(800), random(600), 15, 15);
-     ellipse(random(900), random(600), 15, 15);
-     ellipse(random(900), random(600), 15, 15);
-     ellipse(random(900), random(600), 15, 15);
-     ellipse(random(900), random(600), 15, 15);
-     ellipse(random(1500), random(600), 15, 15);
-     ellipse(random(1500), random(500), 15, 15);
-     ellipse(random(1500), random(600), 15, 15);
-     ellipse(random(1500), random(600), 15, 15);
-     ellipse(random(1400), random(600), 15, 15);
-     ellipse(random(1400), random(600), 15, 15);
-     ellipse(random(1400), random(600), 15, 15);
-     ellipse(random(1400), random(600), 15, 15);
-     ellipse(random(1400), random(600), 15, 15);
-     ellipse(random(1400), random(600), 15, 15);
-     pop();
+    //fill(600);
+    //textSize(24);
+    fill('#8fffff');
+    //ellipse(600, 400, 20, 20);
+    ellipse(random(200), random(400), 15, 15);
+    ellipse(random(300), random(500), 15, 15);
+    ellipse(random(400), random(400), 15, 15);
+    ellipse(random(500), random(500), 15, 15);
+    ellipse(random(600), random(600), 15, 15);
+    ellipse(random(700), random(600), 15, 15);
+    ellipse(random(800), random(600), 15, 15);
+    ellipse(random(900), random(600), 15, 15);
+    ellipse(random(900), random(600), 15, 15);
+    ellipse(random(900), random(600), 15, 15);
+    ellipse(random(900), random(600), 15, 15);
+    ellipse(random(1500), random(600), 15, 15);
+    ellipse(random(1500), random(500), 15, 15);
+    ellipse(random(1500), random(600), 15, 15);
+    ellipse(random(1500), random(600), 15, 15);
+    ellipse(random(1400), random(600), 15, 15);
+    ellipse(random(1400), random(600), 15, 15);
+    ellipse(random(1400), random(600), 15, 15);
+    ellipse(random(1400), random(600), 15, 15);
+    ellipse(random(1400), random(600), 15, 15);
+    ellipse(random(1400), random(600), 15, 15);
+    pop();
 
     //stroke(map(mouseX,0,600,0,255,true));
     //line(width-mouseX, height- mouseY, width-pmouseX, height-pmouseY); // width - makes it mirror the drawing
     //ellipse(mouseX, mouseY, pmouseX,pmouseY); // pmouseX is previous cursor
     // fill(0);
     //ellipse(mouseX, mouseY, 5,5); // pmouseX is previous cursor
-}
+  }
 
   let targetX = mouseX;
   let dx = targetX - x;
@@ -103,7 +103,7 @@ strokeWidth = noise(noiseOffset) * 5;
   let targetY = mouseY;
   let dy = targetY - y;
   y += dy * easing;
-//fill('#b4f035');
+  //fill('#b4f035');
   ellipse(x, y, 20, 20);
 }
 
@@ -112,8 +112,9 @@ function keyTyped() {
   if (key === 's') {
     // save this image
     saveCanvas('fileName', 'png');
-  } else if(key === 'c'){
+  } else if (key === 'c') {
     //clear the images
+
     clear();
   }
   return false;
